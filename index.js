fe2cb6dc5e889a8a33f2fba474a035e49eefa4a9
Peter Bacenet <peterbacenet@icloud.com>
2021-07-25 21:56:03 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+
+main.remove();
+const newHeader = document.createElement('h1') 
+document.body.appendChild(newHeader)
+newHeader.id = "victory";
+newHeader.innerHTML = "Peter is the champion";
